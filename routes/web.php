3feae034fcfd29d5ab5b3db2e3ffa8aee2273859
auliya <aulyamalidi@gmail.com>
2025-05-20 22:17:02 +0700

    Tugas Week 11

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,43 @@ Route::get('blog', function () {
 
 Route::get('hello', [Coba::class, 'helloworld']);
 //Route::get('hello',App\Http\Controllers\Coba::helloworld);
+
+Route::get('bootstrap1', function () {
+	return view('bootstrap1');
+});
+
+Route::get('bootstrap2', function () {
+	return view('bootstrap2');
+});
+
+Route::get('js1', function () {
+	return view('js1');
+});
+
+Route::get('js2', function () {
+	return view('js2');
+});
+
+Route::get('kode6', function () {
+	return view('kode6');
+});
+
+Route::get('linktree', function () {
+	return view('linktree');
+});
+
+Route::get('shop', function () {
+	return view('shop');
+});
+
+Route::get('pertama', function () {
+	return view('pertama');
+});
+
+Route::get('index', function () {
+	return view('index');
+});
+
+Route::get('frontend', function () {
+	return view('frontend');
+});
